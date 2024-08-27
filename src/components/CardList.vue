a0f51c5364ade131e715ee5f5639126c28cc8c85
Ruslan Khaibaliev <nastycoocher@gmail.com>
2024-08-27 22:42:22 +0300

    commit

---
+++ src/components/CardList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      :price="12999"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      isAded
+      isFavorite
+      :onClickAdd="onClickAdd" />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
+
+<script setup>
+import Card from "./Card.vue";
+
+const onClickAdd = () => {
+  alert("add");
+};
+</script>
+
+<style></style>
