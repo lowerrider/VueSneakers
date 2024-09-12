6b3c6705de34b771a2d3c67181a533e4a0969c87
Хайбалиев Руслан Мадиевич <Hajbaliev.RM@OMP.LOCAL>
2024-09-12 11:24:40 +0300

    ”add”

--- src/components/InfoBlock.vue
+++ src/components/InfoBlock.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="flex flex-col items-center text-center max-w-72 mx-auto">
-    <img :src="imageUrl" alt="Info image" height="80" width="70" />
+    <img
+      :src="`/VueSneakers${imageUrl}`"
+      alt="Info image"
+      height="80"
+      width="70"
+    />
     <h2 class="mt-4 text-2xl font-medium">{{ title }}</h2>
     <p class="text-gray-400 mt-2">{{ description }}</p>
   </div>
